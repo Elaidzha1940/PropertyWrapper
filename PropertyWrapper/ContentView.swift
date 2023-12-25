8edaee62dc1a34815a0d77e59995ece983a714dc
Elaidzha1940 <eli080293@gmail.com>
2023-12-25 17:24:09 +0300

    PropertyWrapperApp

---
+++ PropertyWrapper/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: PropertyWrapper
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PropertyWrapper/PropertyWrapperApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: PropertyWrapper
+//  File: PropertyWrapperApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
