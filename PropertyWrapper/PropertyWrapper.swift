f54d95f2117cbf4d71de1bbb65e9576c3ca0722f
Elaidzha1940 <eli080293@gmail.com>
2023-12-26 19:15:53 +0300

    Add extension for FileManager

--- PropertyWrapper/PropertyWrapper.swift
+++ PropertyWrapper/PropertyWrapper.swift
@@ -9,75 +9,66 @@
 
 import SwiftUI
 
-struct FileManagerProperty {
-    var title: String
+extension FileManager {
     
-    private var path: URL {
+    static func documentsPath() -> URL {
         FileManager.default
             .urls(for: .documentDirectory, in: .userDomainMask)
             .first!
             .appending(path: "custom_title.txt")
     }
+}
+
+struct FileManagerProperty: DynamicProperty {
+    @State var title: String
     
-    mutating func load() {
+    init(title: String) {
         do {
-            title = try String(contentsOf: path, encoding: .utf8)
+            self.title = try String(contentsOf: FileManager.documentsPath(), encoding: .utf8)
             print("Sucсess read")
         } catch {
+            self.title = "Starting text"
             print("Error read: \(error)")
+        }
+    }
+    
+    
+    
+    func save(newValue: String) {
+        do {
+            // When atomically is set true, it means that the data will be written to a temporary file first.
+            // When atomically is set false, the data is written directly to the specified file path.
+            
+            try newValue.write(to: FileManager.documentsPath(), atomically: false, encoding: .utf8)
+            title = newValue
+            print("Sucсess saved")
+        } catch {
+            print("Error saving: \(error)")
         }
     }
 }
 
 struct PropertyWrapper: View {
-    @State private var title: String = "Starting title"
+    //    @State private var title: String = "Starting title"
+    var fileManagerProperty = FileManagerProperty(title: FileManager.debugDescription())
     
     var body: some View {
         
         VStack(spacing: 40) {
-            Text(title)
+            Text(fileManagerProperty.title)
                 .font(.system(size: 30, weight: .bold, design: .rounded))
             
             Button("Press me 1") {
-                setTitle(newValue: "title 1")
+                fileManagerProperty.save(newValue: "title 1")
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
             
             Button("Press me 2") {
-                setTitle(newValue: "title 2")
+                fileManagerProperty.save(newValue: "title 2")
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
-        }
-        .onAppear {
-            
-        }
-    }
-    private func setTitle(newValue: String) {
-        let uppercased = newValue.uppercased()
-        title = uppercased
-        save(newValue: uppercased)
-    }
-    
-    
-    private var path: URL {
-        FileManager.default
-            .urls(for: .documentDirectory, in: .userDomainMask)
-            .first!
-            .appending(path: "custom_title.txt")
-    }
-    
-    private func save(newValue: String) {
-        do {
-            // When atomically is set true, it means that the data will be written to a temporary file first.
-            // When atomically is set false, the data is written directly to the specified file path.
-            
-            try newValue.write(to: path, atomically: false, encoding: .utf8)
-            print(NSHomeDirectory())
-            print("Sucсess saved")
-        } catch {
-            print("Error saving: \(error)")
         }
     }
 }
