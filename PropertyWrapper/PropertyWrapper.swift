2a4397040f1182182241143e83b9de2a4cc8dd11
Elaidzha1940 <eli080293@gmail.com>
2023-12-26 17:29:06 +0300

    Update PropertyWrapperApp

--- PropertyWrapper/PropertyWrapper.swift
+++ PropertyWrapper/PropertyWrapper.swift
@@ -8,6 +8,7 @@
 //  */
 
 import SwiftUI
+import Foundation
 
 struct PropertyWrapper: View {
     @State private var title: String = "Starting title"
@@ -18,41 +19,42 @@ struct PropertyWrapper: View {
             Text(title)
                 .font(.system(size: 30, weight: .bold, design: .rounded))
             
-            Button("Press me1") {
+            Button("Press me 1") {
                 setTitle(newValue: "title 1")
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
             
-            Button("Press me2") {
+            Button("Press me 2") {
                 setTitle(newValue: "title 2")
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
         }
     }
-}
-
-private func setTitle(newValue: String) {
-    title = newValue.uppercased()
-}
-
-private func path: URL {
-    FileManager.default
-        .urls(for: .documentDirectory, in: .userDomainMask)
-        .first!
-        .appending(path: "custom_title.txt")
-}
-
-private func save(newValue: String) {
-    do {
-        // When atomically is set true, it means that the data will be written to a temporary file first.
-        // When atomically is set false, the data is written directly to the specified file path.
-        
-        try newValue.write(to: path(), atomically: false, encoding: utf8)
-        print("Sucsess")
-    } catch {
-        
+    private func setTitle(newValue: String) {
+        title = newValue.uppercased()
+        save(newValue: newValue)
+    }
+    
+    
+    private var path: URL {
+        FileManager.default
+            .urls(for: .documentDirectory, in: .userDomainMask)
+            .first!
+            .appending(path: "custom_title.txt")
+    }
+    
+    private func save(newValue: String) {
+        do {
+            // When atomically is set true, it means that the data will be written to a temporary file first.
+            // When atomically is set false, the data is written directly to the specified file path.
+            
+            try newValue.write(to: path, atomically: false, encoding: .utf8)
+            print("Sucсess saved")
+        } catch {
+            print("Error saving: \(error)")
+        }
     }
 }
 
