482a93286520f91d688cd14f2d599bfc0188a9f8
Elaidzha1940 <eli080293@gmail.com>
2023-12-26 20:23:24 +0300

    Add @propertyWrapper / @AppStorage

--- PropertyWrapper/PropertyWrapper.swift
+++ PropertyWrapper/PropertyWrapper.swift
@@ -11,18 +11,20 @@ import SwiftUI
 
 extension FileManager {
     
-    static func documentsPath() -> URL {
+    static func documentsPath(key: String) -> URL {
         FileManager.default
             .urls(for: .documentDirectory, in: .userDomainMask)
             .first!
-            .appending(path: "custom_title.txt")
+            .appending(path: "\(key).txt")
     }
 }
 
+@propertyWrapper
 struct FileManagerProperty: DynamicProperty {
     @State private var title: String
+    let key: String
     
-    var currentValue: String {
+    var wrappedValue: String {
         get {
             title
         }
@@ -31,9 +33,10 @@ struct FileManagerProperty: DynamicProperty {
         }
     }
     
-    init(title: String) {
+    init(key: String) {
+        self.key = key
         do {
-            self.title = try String(contentsOf: FileManager.documentsPath(), encoding: .utf8)
+            self.title = try String(contentsOf: FileManager.documentsPath(key: key), encoding: .utf8)
             print("Sucсess read")
         } catch {
             self.title = "Starting text"
@@ -48,7 +51,7 @@ struct FileManagerProperty: DynamicProperty {
             // When atomically is set true, it means that the data will be written to a temporary file first.
             // When atomically is set false, the data is written directly to the specified file path.
             
-            try newValue.write(to: FileManager.documentsPath(), atomically: false, encoding: .utf8)
+            try newValue.write(to: FileManager.documentsPath(key: key), atomically: false, encoding: .utf8)
             title = newValue
             print("Sucсess saved")
         } catch {
@@ -58,23 +61,29 @@ struct FileManagerProperty: DynamicProperty {
 }
 
 struct PropertyWrapper: View {
-    @State private var title: String = "Starting title"
-    var fileManagerProperty = FileManagerProperty(title: FileManager.debugDescription())
+    //    @State private var title: String = "Starting title"
+    //    var fileManagerProperty = FileManagerProperty(title: FileManager.debugDescription())
+    @AppStorage("title_key") private var title3 = ""
+    @FileManagerProperty(key: "custom_title_1") private var title: String
+    @FileManagerProperty(key: "custom_title_2") private var title2: String
     
     var body: some View {
         
         VStack(spacing: 40) {
-            Text(fileManagerProperty.currentValue)
+            Text(title)
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+            Text(title2)
                 .font(.system(size: 30, weight: .bold, design: .rounded))
             
+            
             Button("Press me 1") {
-                fileManagerProperty.currentValue = "title 1"
+                title = "title 1"
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
             
             Button("Press me 2") {
-                fileManagerProperty.currentValue = "title 2"
+                title = "title 2"
             }
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .foregroundColor(.mint)
