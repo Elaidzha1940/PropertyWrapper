d9070d11df037a317cf3f3efe620c0a9877cb63c
Elaidzha1940 <eli080293@gmail.com>
2023-12-30 01:21:05 +0300

    PropertyWrapper2

---
+++ PropertyWrapper/PropertyWrapper2.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: PropertyWrapper
+//  File: PropertyWrapper2.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct PropertyWrapper2: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    PropertyWrapper2()
+}
